9805c0d4ff2338356e139c41ff052ca763dbb44b
andrivash <andr.ivas12@gmail.com>
2017-08-18 23:08:24 +0300

    hide new operator

--- src/js/app.js
+++ src/js/app.js
@@ -3,9 +3,14 @@ import Timer from './roundtime'
 console.log('app.js');
 
 const seconds = 6;
-const time_stmp = seconds + '000'
+const time_stmp1 = seconds + '000';
+const time_stmp2 = 10 + '000'
 //const time_stmp = seconds + '0'
 
-const timer = new Timer('#inner-timer', time_stmp)
-timer.init();
-//timer.init('inner-timer', time_stmp);
+
+const timer1 = Timer('#timer1', time_stmp1);
+const timer2 = Timer('#timer2', time_stmp2);
+//const timer2 = new Timer('#timer2', time_stmp2)
+timer1.init();
+timer2.init();
+//timer2.init('#timer2', time_stmp2);
--- src/js/roundtime.js
+++ src/js/roundtime.js
@@ -1,46 +1,55 @@
 import '../styles/roundtime.css'
 
+
+
 class RoundTime {
+    
     constructor(containerId, time) {
+        if (!(this instanceof RoundTime)) { 
+            return new RoundTime(containerId, time);
+        }
+        this.timerName = containerId;
         this.container = document.querySelector(containerId);
         this.time = time;
-        this._timeInterval = null;
-        this._deadline = null;
 
-        this.daysElem = null;
-        this.hoursElem = null;
-        this.minutesElem = null;
-        this.secondsElem = null;
-
-        this.circleDay = null;
-        this.circleHour = null;
-        this.circleMin = null;
-        this.circleSec = null;
         
-        // this.rt__sign-day = null;
-        // this.rt__sign-hour = null;
-        // this.rt__sign-min = null;
+        this._privateData = {
+            timeInterval: null,
+            deadline: null,
+            
+            daysElem: null,
+            hoursElem: null,
+            minutesElem: null,
+            secondsElem: null,
+
+            circleDay:null,
+            circleHour: null,
+            circleMin: null,
+            circleSec: null
+        }
+        
     }
+
     init() {
-        this._deadline = new Date(Date.parse(new Date()) + parseInt(this.time));
-        console.log('timer start');
+        this._privateData.deadline = new Date(Date.parse(new Date()) + parseInt(this.time));
+        console.log(`timer ${this.timerName} start`);
         this.addTemplate();
 
-        this.daysElem = this.container.querySelector('#rt-day'),
-        this.hoursElem = this.container.querySelector('#rt-hour'),
-        this.minutesElem = this.container.querySelector('#rt-min'),
-        this.secondsElem = this.container.querySelector('#rt-sec'),
-        this.circleDay = this.container.querySelector('.rt__day'),
-        this.circleHour = this.container.querySelector('.rt__hour'),
-        this.circleMin = this.container.querySelector('.rt__min'),
-        this.circleSec = this.container.querySelector('.rt__sec');
-        
+        this._privateData.daysElem = this.container.querySelector('.rt-day'),
+        this._privateData.hoursElem = this.container.querySelector('.rt-hour'),
+        this._privateData.minutesElem = this.container.querySelector('.rt-min'),
+        this._privateData.secondsElem = this.container.querySelector('.rt-sec'),
+        this._privateData.circleDay = this.container.querySelector('.rt__day'),
+        this._privateData.circleHour = this.container.querySelector('.rt__hour'),
+        this._privateData.circleMin = this.container.querySelector('.rt__min'),
+        this._privateData.circleSec = this.container.querySelector('.rt__sec');
+
         this.initializeClock();
     }
 
     initializeClock() {
         this.updateClock();
-        this._timeInterval = setInterval(this.updateClock.bind(this), 1000);
+        this._privateData.timeInterval = setInterval(() => {this.updateClock()}, 1000);
     }
 
     updateClock() {
@@ -53,7 +62,7 @@ class RoundTime {
         }
     }
     timeRemaining() {
-        const timeRemain = Date.parse(this._deadline) - Date.parse(new Date()),
+        const timeRemain = Date.parse(this._privateData.deadline) - Date.parse(new Date()),
             seconds = Math.floor((timeRemain / 1000) % 60),
             minutes = Math.floor((timeRemain / 1000 / 60) % 60),
             hours = Math.floor((timeRemain / (1000 * 60 * 60)) % 24),
@@ -91,24 +100,21 @@ class RoundTime {
             s: ('0' + timeRemain.seconds).slice(-2)
         };
         
-        this.daysElem.innerHTML = timeRemain.days;
-        this.hoursElem.innerHTML = timeRemain.hours;
-        this.minutesElem.innerHTML = timeRemain.minutes;
-        this.secondsElem.innerHTML = timeRemain.seconds;
-        this.circleDay.style.strokeDashoffset = timeRemain.off_days + 'px';
-        this.circleHour.style.strokeDashoffset = timeRemain.off_hours + 'px';
-        this.circleMin.style.strokeDashoffset = timeRemain.off_min + 'px';
-        this.circleSec.style.strokeDashoffset = timeRemain.off_sec + 'px';  
-
-
-
+        this._privateData.daysElem.innerHTML = timeRemain.days;
+        this._privateData.hoursElem.innerHTML = timeRemain.hours;
+        this._privateData.minutesElem.innerHTML = timeRemain.minutes;
+        this._privateData.secondsElem.innerHTML = timeRemain.seconds;
+        this._privateData.circleDay.style.strokeDashoffset = timeRemain.off_days + 'px';
+        this._privateData.circleHour.style.strokeDashoffset = timeRemain.off_hours + 'px';
+        this._privateData.circleMin.style.strokeDashoffset = timeRemain.off_min + 'px';
+        this._privateData.circleSec.style.strokeDashoffset = timeRemain.off_sec + 'px';  
 
         console.log(`hours: ${data.h}, minutes: ${timeRemain.minutes}, seconds: ${data.s}`);
     }
     stopTimer() {
-        if(this._timeInterval !== null) {
-            clearInterval(this._timeInterval);
-            console.log('timer stop');
+        if(this._privateData.timeInterval !== null) {
+            clearInterval(this._privateData.timeInterval);
+            console.log(`timer ${this.timerName} stop`);
             // return new Promise((resolve, reject)=> {
             //     resolve('timer stop')
             // }, () => {
@@ -124,32 +130,32 @@ class RoundTime {
                 <g>
                     <circle class="rt__back" transform="rotate(-90, 45, 45)" r="40" cx="45" cy="45"></circle>
                     <circle class="rt__front rt__day" transform="rotate(-90, 45, 45)" r="40" cx="45" cy="45"></circle>
-                    <text class="rt__sign" id ="rt-day" x="50%" y="50%" text-anchor="middle"  dy="-5%" >0</text>
-                    <text class="rt__desq" id ="rt-day-text" x="50%" y="50%" text-anchor="middle"  dy="15%">дней</text>
+                    <text class="rt__sign rt-day" x="50%" y="50%" text-anchor="middle"  dy="-5%" >0</text>
+                    <text class="rt__desq rt-day-text" x="50%" y="50%" text-anchor="middle"  dy="15%">дней</text>
                 </g>
             </svg>
             <svg class="rt__circle rt__circle_h" width="90" height="90" viewbox="0 0 90 90">
                 <g>
                     <circle class="rt__back" transform="rotate(-90, 45, 45)" r="40" cx="45" cy="45"></circle>
                     <circle class="rt__front rt__hour" transform="rotate(-90, 45, 45)" r="40" cx="45" cy="45"></circle>
-                    <text class="rt__sign" id ="rt-hour" x="50%" y="50%" text-anchor="middle"  dy="-5%" >0</text>
-                    <text class="rt__desq" id ="rt-hour-text" x="50%" y="50%" text-anchor="middle"  dy="15%">часов</text>
+                    <text class="rt__sign rt-hour" x="50%" y="50%" text-anchor="middle"  dy="-5%" >0</text>
+                    <text class="rt__desq rt-hour-text" x="50%" y="50%" text-anchor="middle"  dy="15%">часов</text>
                 </g>
             </svg>
             <svg class="rt__circle rt__circle_m" width="90" height="90" viewbox="0 0 90 90">
                 <g>
                     <circle class="rt__back" transform="rotate(-90, 45, 45)" r="40" cx="45" cy="45"></circle>
                     <circle class="rt__front rt__min" transform="rotate(-90, 45, 45)" r="40" cx="45" cy="45"></circle>
-                    <text class="rt__sign" id ="rt-min" x="50%" y="50%" text-anchor="middle"  dy="-5%" >0</text>
-                    <text class="rt__desq" id ="rt-min-text" x="50%" y="50%" text-anchor="middle"  dy="15%">минут</text>
+                    <text class="rt__sign rt-min" x="50%" y="50%" text-anchor="middle"  dy="-5%" >0</text>
+                    <text class="rt__desq rt-min-text" x="50%" y="50%" text-anchor="middle"  dy="15%">минут</text>
                 </g>
             </svg>
             <svg class="rt__circle rt__circle_s" width="90" height="90" viewbox="0 0 90 90">
                 <g>
                     <circle class="rt__back" transform="rotate(-90, 45, 45)" r="40" cx="45" cy="45"></circle>
                     <circle class="rt__front rt__sec" transform="rotate(-90, 45, 45)" r="40" cx="45" cy="45"></circle>
-                    <text class="rt__sign" id ="rt-sec" x="50%" y="50%" text-anchor="middle"  dy="-5%" >0</text>
-                    <text class="rt__desq" id ="rt-sec-text" x="50%" y="50%" text-anchor="middle"  dy="15%">секунд</text>
+                    <text class="rt__sign rt-sec" x="50%" y="50%" text-anchor="middle"  dy="-5%" >0</text>
+                    <text class="rt__desq rt-sec-text" x="50%" y="50%" text-anchor="middle"  dy="15%">секунд</text>
                 </g>
             </svg>
         `;
@@ -160,4 +166,8 @@ class RoundTime {
     }
 }
 
-export default RoundTime;
+
+//const roundTime = RoundTime;
+//Foo = function(...args) { return new _old(...args) };
+//export default RoundTime;
+export default function(...args) { return new RoundTime(...args) };
